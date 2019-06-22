a947eadddf9e8937fff0195e8bd2cccd4289d05e
rodrickcalvin <rodrickwamala@gmail.com>
2019-06-22 21:37:51 +0300

    Creates the filter_short function and its description.

---
+++ find_short/Description.md
@@ -0,0 +1,2 @@
+# Find_short
+A function that returns the length of the shortest word in a sentence.
---
+++ find_short/find_short.py
@@ -0,0 +1,3 @@
+def find_short(s):
+    return min(len(x) for x in s.split()) # get the length of every element in a list,
+                                          # return the shortest one length using the min method.
