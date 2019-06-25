a2085d0f2d2803172b272411c016968fc5c5f2c5
rodrickcalvin <rodrickwamala@gmail.com>
2019-06-25 11:25:56 +0300

    Corrects the function indention.

--- Hashtag_Generator/Hashtag_Generator.py
+++ Hashtag_Generator/Hashtag_Generator.py
@@ -3,5 +3,5 @@ import string
 
 
 def generate_hashtag(s):
-      nospaces = re.sub(r'\s','', string.capwords(s)) # Capitalize every letter beginning of a word or letter
-      return False if len('#'+nospaces)>=140 or len(nospaces)<1 else '#'+nospaces
+    nospaces = re.sub(r'\s','', string.capwords(s)) # Capitalize every letter beginning of a word or letter
+    return False if len('#'+nospaces)>=140 or len(nospaces)<1 else '#'+nospaces
